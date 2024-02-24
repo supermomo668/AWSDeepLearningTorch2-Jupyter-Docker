004418b488403db349c5721189a88077211c8bc4
supermomo668 <mymomo119966.mm@gmail.com>
2024-02-23 21:28:41 -0800

    fixed gcp install script

--- cloud-tools-setup/gcp_install.sh
+++ cloud-tools-setup/gcp_install.sh
@@ -1,10 +1,10 @@
-sudo apt update
+apt update
 curl -O https://dl.google.com/dl/cloudsdk/channels/rapid/downloads/google-cloud-cli-451.0.1-linux-x86_64.tar.gz
-sudo tar -xf google-cloud-cli-451.0.1-linux-x86_64.tar.gz -C /usr/local
-sudo ./google-cloud-sdk/install.sh --quiet
+tar -xf google-cloud-cli-451.0.1-linux-x86_64.tar.gz -C /usr/local
+/usr/local/google-cloud-sdk/install.sh --quiet
 # ./google-cloud-sdk/bin/gcloud init
 echo "export PATH=$PATH:/usr/local/google-cloud-sdk/bin" >> ~/.bashrc
 . ~/.bashrc
-gcloud components update
-gcloud components install kubectl
-gcloud components install gke-gcloud-auth-plugin
+gcloud components update --quiet
+gcloud components install kubectl --quiet
+gcloud components install gke-gcloud-auth-plugin --quiet
